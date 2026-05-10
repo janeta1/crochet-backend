34d5bdcd0590baed1669a6f4cf92619e77daa1d4
janeta1 <janetagrigoras@gmail.com>
2026-05-10 20:06:46 +0300

    Fix rows issues in projects

--- routes/projects.js
+++ routes/projects.js
@@ -383,8 +383,8 @@ router.put("/:id", authenticate, requireAdmin, (req, res) => {
           req.params.id,
           part.name,
           part.quantity || 1,
-          part.total_rows || 0,
-          part.completed_rows || 0,
+          parseInt(part.totalRows) || 0,
+          parseInt(part.completedRows) || 0,
         );
       });
     }
