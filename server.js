e164fae4189d96b1b1ec491dd7ad2431e4894eb6
janeta1 <janetagrigoras@gmail.com>
2026-05-10 15:21:33 +0300

    Setup swagger

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import tokenRoutes from "./routes/token.js";
 import projectsRoutes from "./routes/projects.js";
 import sessionsRoutes from "./routes/sessions.js";
 import yarnsRoutes from "./routes/yarns.js";
+import swagger from "./swagger.js";
 
 dotenv.config();
 
@@ -21,10 +22,14 @@ app.use("/projects", projectsRoutes);
 app.use("/sessions", sessionsRoutes);
 app.use("/yarns", yarnsRoutes);
 
+// Swagger API documentation
+app.use("/api-docs", swagger.swaggerUi.serve, swagger.swaggerUi.setup(swagger.swaggerSpec));
+
 app.get("/", (req, res) => {
   res.json({ message: "Stitchbook API is running!" });
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`API documentation available at http://localhost:${PORT}/api-docs`);
 });
---
+++ swagger.js
@@ -0,0 +1,34 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Stitchbook API",
+      version: "1.0.0",
+      description: "REST API for managing crochet projects and yarn stash",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Local development server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ["./routes/*.js"], // Path to the API route files
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default { swaggerSpec, swaggerUi };
